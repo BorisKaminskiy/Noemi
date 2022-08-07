5f4ba1021965ad6adf10eacc8c2a8708cbc95629
Boris Kaminskiy <93283934+BorisKaminskiy@users.noreply.github.com>
2022-08-07 11:50:31 +0300

    Update burger.js

--- JS/burger.js
+++ JS/burger.js
@@ -1,10 +1,12 @@
 const burgerButton = document.querySelector(".header-burger-button");
 const burgerNavigation = document.querySelector(".header-navigation");
+const darkTheme = document.querySelector(".dark-theme");
 
 function burgerMenu() {
   burgerButton.addEventListener("click", () => {
     burgerButton.classList.toggle("header-burger-button-active");
     burgerNavigation.classList.toggle("header-navigation-active");
+    darkTheme.classList.toggle("dark-theme-active");
   })
 }
 
